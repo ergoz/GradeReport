6da37b7bc56386a0b646c6174204508ea3da3bd3
robotsrulz <robots.rulz@gmail.com>
2013-11-11 03:23:43 +0400

    added icons for settings, populated TODO

--- gen/com/shinymetal/gradereport/R.java
+++ gen/com/shinymetal/gradereport/R.java
@@ -29,8 +29,9 @@ public final class R {
         public static final int ic_action_person=0x7f020002;
         public static final int ic_action_previous_item=0x7f020003;
         public static final int ic_action_refresh=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int shinymetal=0x7f020006;
+        public static final int ic_action_settings=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int shinymetal=0x7f020007;
     }
     public static final class id {
         public static final int action_next_week=0x7f0a000d;
