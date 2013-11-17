eca63f806a1c6cb417935ded34f6a4af7029dc89
robotsrulz <robots.rulz@gmail.com>
2013-11-17 04:29:25 +0400

    published as alpha 4 on play market

--- gen/com/shinymetal/gradereport/R.java
+++ gen/com/shinymetal/gradereport/R.java
@@ -31,7 +31,7 @@ public final class R {
         public static final int ic_action_refresh=0x7f020004;
         public static final int ic_action_settings=0x7f020005;
         public static final int ic_launcher=0x7f020006;
-        public static final int shinymetal=0x7f020007;
+        public static final int splash=0x7f020007;
     }
     public static final class id {
         public static final int ScrollViewDetail=0x7f0a0006;
